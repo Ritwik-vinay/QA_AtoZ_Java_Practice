d02d532d19978a7a1a367b05209b6228d6f93834
ritwik vinay <ritwikvinay38@gmail.com>
2025-01-11 16:57:15 +0530

    Task

---
+++ src/java_B/class14.java
@@ -0,0 +1,20 @@
+package java_B;
+
+public class class14 {
+    public static void main(String[] args) {
+        System.out.printf("Table of  9:\n");
+        System.out.printf("9 x 1 = 9\n");
+        System.out.printf("9 x 2 = 18\n");
+        System.out.printf("9 x 3 = 27\n");
+        System.out.printf("9 x 4 = 36\n");
+        System.out.printf("9 x 5 = 45\n");
+        System.out.printf("9 x 6 = 54\n");
+        System.out.printf("9 x 7 = 63\n");
+        System.out.printf("9 x 8 = 72\n");
+        System.out.printf("9 x 9 = 81\n");
+        System.out.printf("9 x 10 = 90\n");
+
+
+
+    }
+}
