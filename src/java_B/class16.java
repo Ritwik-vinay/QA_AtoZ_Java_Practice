e48d4123b375b7f4094054a2cd10e44a3b9f0b38
ritwik vinay <ritwikvinay38@gmail.com>
2025-01-16 09:34:11 +0530

    initial commit

---
+++ src/java_B/class16.java
@@ -0,0 +1,20 @@
+package java_B;
+
+public class class16 {
+
+    public static void main(String[] args) {
+        class16_1 m = new class16_1();
+        System.out.println("run main 1");
+        m.n();
+    }
+
+
+}
+
+class class16_1 {
+    public static void n() {
+
+        System.out.println("run main 2");
+
+    }
+}
