22951314c8da7e52c6035424502ceaf7a68d6cb2
ritwik vinay <ritwikvinay38@gmail.com>
2025-02-05 09:00:39 +0530

    Commit Task :  4 FEB Task Strings

---
+++ src/Java_F_Task/class62_Arrays.java
@@ -0,0 +1,17 @@
+package Java_F_Task;
+
+public class class62_Arrays {
+    public static void main(String[] args) {
+        //basic Arrays
+        int a[] = new int[5];
+        a[0] = 5;
+        a[1] = 4;
+        a[2] = 3;
+        a[3] = 2;
+        a[4] = 1;
+
+        for (int i = 1; i <= a.length; i++) {
+            System.out.println(i);
+        }
+    }
+}
