e34ed5b925dcfe6c64532480a33bd1264d022447
ritwik vinay <ritwikvinay38@gmail.com>
2025-02-06 23:43:20 +0530

    Commit Task :  5 FEB Task while

---
+++ src/Java_G_Task/class72_tableof5.java
@@ -0,0 +1,13 @@
+package Java_G_Task;
+
+public class class72_tableof5 {
+    public static void main(String[] args) {
+        int num = 5;
+        int i = 0;
+        while (i <= 9) {
+            i++;
+            int n = num * i;
+            System.out.println(n);
+        }
+    }
+}
