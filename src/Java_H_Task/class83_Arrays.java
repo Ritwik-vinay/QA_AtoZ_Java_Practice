c6af0bb47beb3ac9780eb4479d2833d12db77e75
ritwik vinay <ritwikvinay38@gmail.com>
2025-02-10 09:04:41 +0530

    Commit Task :  7 FEB Task Arrays

---
+++ src/Java_H_Task/class83_Arrays.java
@@ -0,0 +1,13 @@
+package Java_H_Task;
+
+public class class83_Arrays {
+    public static void main(String[] args) {
+        int[] i = {1, 2, 3, 4, 5, 6};
+        int[] j = {4, 5, 6, 7, 8, 9};
+        for (int k = 1; k <= i.length; k++) {
+            System.out.println(k);
+        }
+
+
+    }
+}
