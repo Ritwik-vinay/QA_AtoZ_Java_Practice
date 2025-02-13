576f9ea77d446178c791226a3a40f7e3fa41973b
ritwik vinay <ritwikvinay38@gmail.com>
2025-02-13 07:53:16 +0530

    Commit Task :  13 FEB Task Arrays

---
+++ src/Java_I_Task/class89.java
@@ -0,0 +1,9 @@
+package Java_I_Task;
+
+public class class89 {
+    public static void main(String[] args) {
+        String s = "50";
+        int a = Integer.parseInt(s);
+        System.out.println(a);
+    }
+}
