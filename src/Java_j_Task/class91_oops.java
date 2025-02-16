dd1552ec6e65cc04c7ed21915ef805a5415e4382
ritwik vinay <ritwikvinay38@gmail.com>
2025-02-16 23:09:02 +0530

    Commit Task :  16 FEB Oops Constructor

---
+++ src/Java_j_Task/class91_oops.java
@@ -0,0 +1,32 @@
+package Java_j_Task;
+
+public class class91_oops {
+    public static void main(String[] args) {
+        vehicle v = new vehicle();
+        v.speed();
+        v.speed(230);
+        vehicle v2 = new vehicle(10);
+
+
+    }
+
+}
+
+class vehicle {
+    vehicle() {
+        System.out.println("DC");
+    }
+
+    vehicle(int a) {
+        System.out.println("Parameterized Constructor" + a);
+    }
+
+    void speed() {
+        int speed = 180;
+        System.out.println("Default Method having speed " + speed);
+    }
+
+    void speed(int b) {
+        System.out.println("Parameterized Method speed " + b);
+    }
+}
