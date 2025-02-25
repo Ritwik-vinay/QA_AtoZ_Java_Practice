3bc4bb44d40f5f9a86849fe26614fd984655daa5
ritwik vinay <ritwikvinay38@gmail.com>
2025-02-25 09:28:45 +0530

    Commit Task :  25 FEB task 2nd

---
+++ src/Java_M_Task/class131_StringBuffer_toString.java
@@ -0,0 +1,10 @@
+package Java_M_Task;
+
+public class class131_StringBuffer_toString {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer();
+
+        String str = sb.append("My name is Ritwik").toString();
+        System.out.println(str);
+    }
+}
